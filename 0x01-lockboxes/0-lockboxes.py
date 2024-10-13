0e6f723ee2c8d92edafb88b5bc67c5d4e3e03376
OGOR-STEPHEN <ogorstephen485@gmail.com>
2024-10-14 00:32:53 +0100

    Third commit

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,12 +1,15 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 
 def canUnlockAll(boxes):
     """
-    Determines if all boxes can be unlocked.
+    Determines if all boxes in a list of boxes can be unlocked.
+
+    Each box may contain keys to unlock other boxes. The first box is unlocked by default,
+    and we use the keys found in each box to unlock others. The goal is to check if
+    all boxes can be unlocked.
 
     Parameters:
-    boxes (list of lists): A list where each element is a list of keys contained in that box.
-                           The box at index 0 is always unlocked.
+    boxes (list of lists): A list where each element represents a box containing a list of keys.
 
     Returns:
     bool: True if all boxes can be unlocked, False otherwise.
@@ -21,16 +24,28 @@ def canUnlockAll(boxes):
     >>> canUnlockAll([[1, 2], [0], [0, 1]])
     False
     """
+
+    # Total number of boxes
     n = len(boxes)
-    unlocked = [False] * n  # Track unlocked boxes
-    unlocked[0] = True  # Box 0 is always unlocked
-    keys = [0]  # Start with the keys from box 0
 
+    # A list to track which boxes are unlocked. Initially, only box 0 is unlocked.
+    unlocked = [False] * n
+    unlocked[0] = True  # Box 0 is unlocked by default
+
+    # Stack of keys we have collected, starting with box 0's keys
+    keys = [0]
+
+    # While there are still keys to process
     while keys:
-        current_key = keys.pop()  # Get the next key to process
-        for key in boxes[current_key]:
-            if key < n and not unlocked[key]:  # Check if key is valid and unlock new boxes
-                unlocked[key] = True  # Unlock the box
-                keys.append(key)  # Add keys from the newly unlocked box
+        # Take one key to try to unlock the corresponding box
+        current_key = keys.pop()
 
-    return all(unlocked)  # Check if all boxes are unlocked
+        # Try to unlock all boxes corresponding to the keys in the current box
+        for key in boxes[current_key]:
+            # Only unlock valid boxes and skip boxes that are already unlocked
+            if key < n and not unlocked[key]:
+                unlocked[key] = True  # Unlock the box
+                keys.append(key)  # Add keys from this newly unlocked box to explore further
+
+    # If all boxes are unlocked, return True; otherwise, return False
+    return all(unlocked)
